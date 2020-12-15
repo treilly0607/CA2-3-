ba8072f33c99123820e6b47bf4dccbd95bf2cc74
treilly0607 <60261903+treilly0607@users.noreply.github.com>
2020-12-15 14:32:51 +0000

    Added button to add employees

---
+++ CA2-15-12-2020/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2_15_12_2020
+{
+    public abstract class Employee
+    {
+        public string FirstName { get; set; } // first name property
+        public string Surname { get; set; } // surname property
+        public abstract decimal CalculateMonthlyPay(decimal MonthlyPay); // abstract class for CalculateMonthlyPay
+
+        public override string ToString()
+        {
+            return string.Format($"{Surname}, {FirstName}");
+        }
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+        public decimal Salary { get; set; } // Salary property
+
+        public override decimal CalculateMonthlyPay(decimal MonthlyPay) // override abstract method for MonthlyPay
+        {
+            MonthlyPay = Salary / 12; // calc to find monthly pay of a Salary
+            return MonthlyPay; // return the MonthlyPay
+        }
+    }
+
+    public class PartTimeEmployee : Employee
+    {
+        public decimal HourlyRate { get; set; } // HourlyRate property
+        public double HoursWorked { get; set; } // HoursWorked property
+
+        public override decimal CalculateMonthlyPay(decimal MonthlyPay) // override abstract method for MonthlyPay
+        {
+            MonthlyPay = HourlyRate * (decimal)HoursWorked; // calc MonthlyRate by HourlyRate and HoursWorked also casted decimal to HoursWorked
+            return MonthlyPay; // return MonthlyPay
+        }
+    }
+}
--- CA2-15-12-2020/MainWindow.xaml.cs
+++ CA2-15-12-2020/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CA2_15_12_2020
 {
@@ -32,7 +20,90 @@ namespace CA2_15_12_2020
         public MainWindow()
         {
             InitializeComponent();
+        }
 
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Employee selectedEmployee = ListBox.SelectedItem as Employee;
+
+            if (selectedEmployee != null) // ensure selected item isnt null
+            {
+                TxtBoxFN.Text = selectedEmployee.FirstName;
+                TxtBoxSn.Text = selectedEmployee.Surname;
+                
+                if (selectedEmployee is FullTimeEmployee)
+                {
+                    //TxtBoxSalary = selectedEmployee.Salary;
+                    RBtnFT.IsChecked = true;
+                    RBtnPT.IsChecked = false;
+                }
+                else if (selectedEmployee is PartTimeEmployee)
+                {
+                    //TxtBoxHR = selectedEmployee.HourlyRate;
+                    //TxtBoxHW = selectedEmployee.HoursWorked;
+                    RBtnPT.IsChecked = true;
+                    RBtnFT.IsChecked = false;
+                }
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FullTimeEmployee emp1 = new FullTimeEmployee(); // fulltime employee object
+            emp1.FirstName = "Kevin";
+            emp1.Surname = "McCallister";
+            emp1.Salary = 45000;
+            FullTimeEmployee emp2 = new FullTimeEmployee(); // fulltime employee object
+            emp2.FirstName = "Alex";
+            emp2.Surname = "Brennan";
+            emp2.Salary = 60000;
+
+            PartTimeEmployee emp3 = new PartTimeEmployee(); // parttime employee object
+            emp3.FirstName = "Martin";
+            emp3.Surname = "FitzGerald";
+            emp3.HourlyRate = 12.50m;
+            emp3.HoursWorked = 45;
+            PartTimeEmployee emp4 = new PartTimeEmployee(); // parttime employee object
+            emp4.FirstName = "Linda";
+            emp4.Surname = "McGettigan";
+            emp4.HourlyRate = 14.50m;
+            emp4.HoursWorked = 48;
+
+            employees.Add(emp1); // added employees to collecion employees
+            employees.Add(emp2);
+            employees.Add(emp3);
+            employees.Add(emp4);
+
+            ListBox.ItemsSource = employees;
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = TxtBoxFN.Text;
+            string surname = TxtBoxSn.Text;
+            string salary = TxtBlockSalary.Text;
+            string hoursWorked = TxtBlockHW.Text;
+            string hourlyRate = TxtBlockHR.Text;
+
+            if (RBtnFT.IsChecked == true && RBtnPT.IsChecked == false) // if statement to check if emp is part time or full time
+            {
+                FullTimeEmployee emp = new FullTimeEmployee(); // fulltime employee object
+                emp.FirstName = firstName;
+                emp.Surname = surname;
+                //emp.Salary = salary;
+
+                employees.Add(emp);
+            }
+            else if (RBtnFT.IsChecked == false && RBtnPT.IsChecked == true)
+            {
+                PartTimeEmployee emp = new PartTimeEmployee(); // parttime employee object
+                emp.FirstName = firstName;
+                emp.Surname = surname;
+                //emp.HourlyRate = hourlyRate;
+                //emp.HoursWorked = hoursWorked; 
+
+                employees.Add(emp);
+            }
         }
     }
 }
